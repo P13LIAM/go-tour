9c974b25c4428da656fae0b6bc4685946d8bdb48
petongz <petong.zuckerberg@gmail.com>
2022-08-14 11:13:10 +0700

    feat: ex5

---
+++ Exercise_5/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (ipaddr IPAddr) String() string {
+	var strs []string
+	for _, v := range ipaddr {
+		strs = append(
+			strs, strconv.Itoa(int(v)))
+	}
+	return strings.Join(strs, ".")
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
