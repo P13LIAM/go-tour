6653ffb29c0b639304a104fe6680e39b025d4d56
petongz <petong.zuckerberg@gmail.com>
2022-08-14 11:42:41 +0700

    feat: ex7

---
+++ Exercise_7/go.mod
@@ -0,0 +1,3 @@
+module github.com/petongz/go-tour/Exercise_7
+
+go 1.19
---
+++ Exercise_7/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader) Read(s []byte) (int, error) {
+	for i := range s {
+		s[i] = 'A'
+	}
+
+	return len(s), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
